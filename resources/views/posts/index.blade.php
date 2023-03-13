97adc7c3cdde6a3ecff8e85f53de6d9b05c44fa8
Gautam Dheer <gautam.litesh@gnail.com>
2023-03-13 18:01:44 +0530

    likes functionality intiated

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-          <h1 class="mb-3 font-bold text-black text-red-600">Add Post</h1>
+            <h1 class="mb-3 font-bold text-black text-red-600">Add Post</h1>
             <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
 
@@ -29,24 +29,23 @@
 
             @if ($posts->count())
                 @foreach ($posts as $post)
-               
                     <a href="" class="font-bold">{{ Str::title($post->user->name) }}</a> <span
                         class="text-gray-500 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                     <div class="mb-4">{{ $post->body }}</div>
 
 
-                    <div class="flex item-center">
-                      <form action="" class="mr-1">
-                        @csrf
-                          <button type="submit" class="text-blue-500">Likes</button>
-                      </form>
-                      <form action="" class="mr-1">
-                        @csrf
-                        <button type="submit" class="text-blue-500">Dislikes</button>
-                      </form>
-      
-                    </div>
+                    <div class="flex item-center mb-4">
+                        <form action="" class="mr-1">
+                            @csrf
+                            <button type="submit" class="text-blue-500">Likes</button>
+                        </form>
+                        <form action="" class="mr-1">
+                            @csrf
+                            <button type="submit" class="text-blue-500">Dislikes</button>
+                        </form>
 
+                        <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                    </div>
                 @endforeach
 
 
